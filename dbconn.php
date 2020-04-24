d781796c7f584f0e2a1678f748b95ec8e91ed7e9
nanziyo.hiro <nan.hiro20@gmail.com>
2020-04-24 22:48:38 +0900

    Delete dbconn.php

--- dbconn.php
+++
@@ -1,7 +0,0 @@
-<?php
- try{
-     $db = new PDO('mysql:dbname=dailyreport;host=127.0.0.1;charset=utf8', 'root', 'パスワード');
-    } catch(PDOException $e){
-    print('DB接続エラー：'. $e->getmessage());
- }
-?>
